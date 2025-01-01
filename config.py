60fe570b29c52932179db98ab5c258d1dcc506b9
Marwin2002 <musicbotid@gmail.com>
2025-01-01 14:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # -------------------------------------------------------------
-API_ID = "22084906"
+API_ID = "27758016"
 # -------------------------------------------------------------
-API_HASH = "4ea89573b2ffa4b66154c7cf8c6fdd48"
+API_HASH = "8d34cfffe27ab461eabbf0091b1a27df"
 # -------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs")
 # -------------------------------------------------------------
-OWNER_ID = getenv("OWNER_ID", 7009601543)
+OWNER_ID = getenv("OWNER_ID", 7224419362)
 # -------------------------------------------------------------
-MONGO_DB_URI = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+MONGO_DB_URI = "mongodb+srv://architect2002:architect2002@cluster0.ccinu.mongodb.net/"
 # -------------------------------------------------------------
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PBX_CHAT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Architect04")
 # -------------------------------------------------------------
-MUST_JOIN = getenv("MUST_JOIN","PBX_CHAT")
+MUST_JOIN = getenv("MUST_JOIN","The_Architect04")
 # -------------------------------------------------------------
